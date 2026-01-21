e0aa62b77faa4fca4ee5d30fe8dcb0a57cd34893
t-tsuchiya <t.tsuchiya@b-breaks.jp>
2026-01-21 10:18:54 +0900

    config fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel'; // ★ここが重要！ /serverless を消す
 
 export default defineConfig({
   output: 'server',
