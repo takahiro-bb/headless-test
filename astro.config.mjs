398f13e7540de11464da976af8e33f1faa51301e
t-tsuchiya <t.tsuchiya@b-breaks.jp>
2026-01-21 10:05:19 +0900

    Fix: Remove /serverless from import path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/';
 
 export default defineConfig({
   output: 'server',
